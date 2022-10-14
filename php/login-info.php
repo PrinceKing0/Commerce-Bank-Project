b0763a2ff98e80b65765143b7ede4ea9ebd5aa06
Jared <jared_lee@outlook.com>
2022-10-14 15:25:36 -0500

    hook signin-form to php and sql.

--- php/login-info.php
+++ php/login-info.php
@@ -1,13 +1,9 @@
 <?php
 
-$loginName = $_POST["loginName"];
-$loginPw = $_POST["loginPw"];
-if(isset($_POST["remember"])) {
-    $remember = 1;
-}
-else {
-    $remember = 0;
-}
+$fName = $_POST["fName"];
+$lName = $_POST["lName"];
+$loginName = $_POST["eMail"];
+$loginPw = password_hash($_POST["pWord"], PASSWORD_DEFAULT);
 
 $host = "localhost";
 $dbname = "classproj_db1";
@@ -20,8 +16,8 @@ if (mysqli_connect_errno()) {
     die("Connection error: " . mysqli_connect_error());
 }
 
-$sql = "INSERT INTO login_info (username, password, remember)
-    VALUES (?, ?, ?)";
+$sql = "INSERT INTO login_info (firstName, lastName, username, password)
+    VALUES (?, ?, ?, ?)";
 
 $stmt = mysqli_stmt_init($conn);
 
@@ -29,10 +25,12 @@ if (! mysqli_stmt_prepare($stmt, $sql)) {
     die(mysqli_error($conn));
 }
 
-mysqli_stmt_bind_param($stmt, "ssi", $loginName, $loginPw, $remember);
+mysqli_stmt_bind_param($stmt, "ssss", $fName, $lName, $loginName, $loginPw);
 
 mysqli_stmt_execute($stmt);
 
 echo "Record Saved";
+//echo $_SERVER['DOCUMENT_ROOT'];
+header("Location: /index.html");
 
 ?>
