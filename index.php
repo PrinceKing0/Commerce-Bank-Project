fa9873ecf99532d31cf4300048935655b507111a
Jared <jared_lee@outlook.com>
2022-10-24 11:34:11 -0500

    grammar and mapping

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ session_start();
 		<div class="container">
 			<div class="row align-items-center justify-content-between">
 				<div class="col-lg-6 text-left">
-					<span class="subheading-white text-white mb-3" data-aos="fade-up">Fundraisers</span>
+					<span class="subheading-white text-white mb-3" data-aos="fade-up">Fundraiser</span>
 					<h1 class="heading text-white mb-2" data-aos="fade-up">Give a helping hand to those who need it!</h1>
 					<p data-aos="fade-up" class=" mb-5 text-white lead text-white-50">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum minima dignissimos hic mollitia eius et quam ducimus maiores eos magni.</p>
 					<p data-aos="fade-up"  data-aos-delay="100">
